7214049798f98fe10ad0f9b947926f796ef91016
Şerefsaid Nurkaya <serefsaid@msn.com>
2023-10-02 20:01:11 +0300

    solved issue #1
    solved it with a basic check system (on v1.1)

---
+++ day10/part2.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+var puzzleInput = fs.readFileSync('input.txt','utf8').split('\n').map(line => line.trim()).map(line =>line.split(' '));//fetching input from txt file
+const createImage = (input)=>{
+    let result;
+    return result;
+}
+console.log(createImage(puzzleInput));
